b01798b835d9038b05a5947f262495b818a1149d
kyr0 <info@aron-homberg.de>
2019-11-10 01:32:24 +0100

    fix: disabled style

--- src/component/mwc-checkbox/mwc-checkbox.tpl.tsx
+++ src/component/mwc-checkbox/mwc-checkbox.tpl.tsx
@@ -2,13 +2,19 @@ import { tsx } from "springtype/web/vdom";
 import { MwcCheckbox } from "./mwc-checkbox";
 
 export default (component: MwcCheckbox) => {
+    
     const classes = ["mdc-checkbox"];
+    const input = <input type="checkbox" class="mdc-checkbox__native-control" id={component.inputId} />;
+    
     if (component.disabled === "true") {
         classes.push("mdc-checkbox--disabled")
+        input.attributes.disabled = "disabled";
     }
+
+
     return <div id={component.formFieldId} class="mdc-form-field">
         <div id={component.checkboxId} class={classes}>
-            <input type="checkbox" class="mdc-checkbox__native-control" id={component.inputId}/>
+            {input}
             <div class="mdc-checkbox__background">
                 <svg class="mdc-checkbox__checkmark"
                      viewBox="0 0 24 24">
