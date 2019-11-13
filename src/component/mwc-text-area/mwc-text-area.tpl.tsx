468b43c2f4071b34b3d0ec2f057f79eecdda35aa
Michael Mannseicher <m.mansi1@gmx.net>
2019-11-13 01:48:58 +0100

    fix: update textfield getting value name type

--- src/component/mwc-text-area/mwc-text-area.tpl.tsx
+++ src/component/mwc-text-area/mwc-text-area.tpl.tsx
@@ -1,10 +1,11 @@
 import {tsx} from 'springtype/web/vdom';
-import {MwcBaseTextField} from "../mwc-base-textfield/mwc-base-text-field";
+import {MwcBaseTextField} from "../mwc-base-textfield";
 import {MwcTextArea} from "./mwc-text-area";
 
 export default (component: MwcTextArea) => {
 
-    return <MwcBaseTextField name={component.name} label={component.label} variant={component.variant}
+    return <MwcBaseTextField ref={{mwcBaseTextFieldRef: component}} name={component.name} label={component.label}
+                             variant={component.variant}
                              ripple={component.ripple} disabled={component.disabled} value={component.value}
                              shaped={component.shaped} textarea={true} fullwidth={component.fullwidth}>
     </MwcBaseTextField>
